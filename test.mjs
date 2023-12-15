7c6e8a781aec216f679fcc5b59d4e11ebc31f0f9
Michael Hampson <michael_hampson@harvard.edu>
2023-12-14 21:59:18 -0500

    Test the assertion functions

---
+++ test.mjs
@@ -0,0 +1,78 @@
+import {
+    assert,
+    assertEq,
+    assertNotEq,
+    assertException,
+    assertNaN,
+    assertInstance,
+    assertFinite,
+    assertInfinite,
+    assertType,
+    assertNotNull,
+    assertNull,
+    assertChildOf,
+    assertNoChildren,
+    testBiker,
+    Pass,
+    Fail,
+} from "./main.mjs";
+
+// Testing our assertions
+
+let tests = [
+    function testAssertions() {
+        let pass = assert(true, "test passes");
+        let fail = assert(false, "test fails");
+
+        if (pass instanceof Pass && fail instanceof Fail) {
+            return new Pass();
+        } else {
+            return new Fail("Basic assertions are not working");
+        }
+    },
+
+    function testEq() {
+        return assertEq("t", "t");
+    },
+
+    function testNotEq() {
+        return assertNotEq("t", "r");
+    },
+
+    function testException() {
+        return assertException(() => {
+            throw new Error("test");
+        });
+    },
+
+    function testNaN() {
+        return assertNaN("test");
+    },
+
+    function testInstance() {
+        class Test {}
+        return assertInstance(new Test(), Test);
+    },
+
+    function testFinite() {
+        return assertFinite(1);
+    },
+
+    function testInfinite() {
+        return assertInfinite(1 / 0);
+    },
+
+    function testType() {
+        return assertType("test", "string");
+    },
+
+    function testNotNull() {
+        return assertNotNull("test");
+    },
+
+    function testNull() {
+        return assertNull(null);
+    },
+];
+
+testBiker(tests);
